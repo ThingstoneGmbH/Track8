2fdcac52b188bf7c3aa86cf49407b81250e5a881
Collin Raddatz <3696742+ColRad@users.noreply.github.com>
2026-09-16 21:49:46 +0200

    Manual for v1.2.2-SNAPSHOT

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f3a9eb63.js',
+                path_to_root + 'searchindex-91d5e25b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
